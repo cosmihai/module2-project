a0a715e645915048215bdad6c517edc625d766f2
cosmihai <cos_mihai23@yahoo.com>
2018-04-23 16:46:37 +0200

    create event problems with date

--- catch-the-light/routes/event.js
+++ catch-the-light/routes/event.js
@@ -8,5 +8,25 @@ const User = require('../models/user');
 const Event = require('../models/event');
 
 router.get('/new', (req, res, next) => {
-  res.render();
+  res.render('pages/event/event-create');
 });
+
+router.post('/', (req, res, next) => {
+  const userId = req.session.user._id;
+  req.body.owner = userId;
+  const location = {
+    type: 'Point',
+    coordinates: [req.body.longitude, req.body.latitude]
+  };
+  req.body.location = location;
+  // req.body.date = new Date(req.body.date);
+
+  const event = new Event(req.body);
+  event.save()
+    .then(() => {
+      res.redirect(`/users/${userId}`);
+    })
+    .catch(next);
+});
+
+module.exports = router;
